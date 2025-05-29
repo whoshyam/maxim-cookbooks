5294acd730b8218cd26c7cd8672ffc2c62b4c544
Akshay Deo <akshay@akshaydeo.com>
2025-05-29 12:31:40 +0530

    chore: adds misc test cases for python observability

--- python/observability-online-eval/manual-tracing/hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from manual-tracing!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ python/observability-online-eval/misc/main.py
@@ -0,0 +1,31 @@
+# This script tests before application exits - we automatically cleanup the logger to avoid any data loss
+import logging
+import uuid
+
+import dotenv
+from maxim import Maxim
+
+logging.basicConfig(level=logging.DEBUG)
+
+dotenv.load_dotenv()
+
+logger = Maxim({"debug": True}).logger({})
+
+
+def create_trace():
+    trace = logger.trace({"id": str(uuid.uuid4()), "name": "test trace"})
+
+    trace.event(str(uuid.uuid4()), "test event")
+
+    span = trace.span({"id": str(uuid.uuid4()), "name": "test span"})
+
+    span.event(str(uuid.uuid4()), "test span event")
+
+    span.end()
+
+    trace.end()
+
+
+if __name__ == "__main__":
+    create_trace()
+    print("Hello, World!")
