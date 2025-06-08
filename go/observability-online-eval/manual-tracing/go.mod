c09f717f82ba16e0843fbee2008736f28bcbc8a5
Akshay Deo <akshay@akshaydeo.com>
2025-06-08 18:39:42 +0530

    chore: fixes maxim-py version (#69)
    * chore: fixes maxim-py version
    
    * Update litellm-one-line-integration.ipynb

--- go/observability-online-eval/manual-tracing/go.mod
+++ go/observability-online-eval/manual-tracing/go.mod
@@ -2,10 +2,8 @@ module github.com/maximhq/maxim-cookbooks/go/observability-online-eval/manual-tr
 
 go 1.21.5
 
-replace github.com/maximhq/maxim-go => ../../../../universe/libs/maxim-go/
-
 require (
 	github.com/google/uuid v1.6.0
-	github.com/joho/godotenv v1.5.1 // indirect	
-	github.com/maximhq/maxim-go v0.1.5 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/maximhq/maxim-go v0.1.7
 )
--- go/observability-online-eval/manual-tracing/main.go
+++ go/observability-online-eval/manual-tracing/main.go
@@ -13,41 +13,44 @@ import (
 var maximClient *maxim.Maxim
 var logger *logging.Logger
 
-func init(){
+func init() {
 	var err error
 	err = godotenv.Overload()
 	if err != nil {
 		log.Println("Warning: Error loading .env file:", err)
 	}
-	maximClient = maxim.Init(&maxim.MaximSDKConfig{Debug: true})	
+	maximClient = maxim.Init(&maxim.MaximSDKConfig{Debug: true})
 	logger, err = maximClient.GetLogger(&logging.LoggerConfig{})
 	if err != nil {
 		log.Fatal("Error initializing logger:", err)
 	}
 }
 
-// In this flow
-// We will create a trace
-// Then add a span to it
-// And then add a generation to the span
-func main(){
-	
-	trace := logger.Trace(&logging.TraceConfig{
-		Id: uuid.New().String(),
-		Name: maxim.StrPtr("test trace"),
-	})
+func logTrace(session *logging.Session) {
+	var trace *logging.Trace
+	if session != nil {
+		trace = session.AddTrace(&logging.TraceConfig{
+			Id:   uuid.New().String(),
+			Name: maxim.StrPtr("test trace"),
+		})
+	} else {
+		trace = logger.Trace(&logging.TraceConfig{
+			Id:   uuid.New().String(),
+			Name: maxim.StrPtr("test trace"),
+		})
+	}
 	span := trace.AddSpan(&logging.SpanConfig{
-		Id: uuid.New().String(),
+		Id:   uuid.New().String(),
 		Name: maxim.StrPtr("test span"),
 	})
 	generation := span.AddGeneration(&logging.GenerationConfig{
-		Id: uuid.New().String(),
-		Name: maxim.StrPtr("test generation"),
+		Id:       uuid.New().String(),
+		Name:     maxim.StrPtr("test generation"),
 		Provider: "openai",
-		Model: "gpt-4o",
+		Model:    "gpt-4o",
 		Messages: []logging.CompletionRequest{
 			{
-				Role: "user",
+				Role:    "user",
 				Content: "Hello, world!",
 			},
 		},
@@ -57,24 +60,47 @@ func main(){
 	})
 	time.Sleep(1 * time.Second)
 	generation.SetResult(map[string]interface{}{
-		"id": uuid.New().String(),
-		"model": "gpt-4o",
+		"id":      uuid.New().String(),
+		"model":   "gpt-4o",
 		"created": time.Now().Unix(),
 		"choices": []map[string]interface{}{
 			{
 				"message": map[string]interface{}{
-					"role": "assistant",
+					"role":    "assistant",
 					"content": "Hello, world!",
 				},
 			},
 		},
 		"usage": map[string]interface{}{
-			"prompt_tokens": 10,
+			"prompt_tokens":     10,
 			"completion_tokens": 10,
-			"total_tokens": 20,
+			"total_tokens":      20,
 		},
-	})	
+	})
 	span.End()
-	trace.End()	
+	trace.End()
+}
+
+func logTraceWithSession() {
+	sessionId := uuid.New().String()
+	session := logger.Session(&logging.SessionConfig{
+		Id:   sessionId,
+		Name: maxim.StrPtr("test session"),
+	})
+	logTrace(session)
+	session.SetFeedback(&logging.Feedback{
+		Score:   3,
+		Comment: maxim.StrPtr("This is a test feedback"),
+	})
+	session.End()
+}
+
+// In this flow
+// We will create a trace
+// Then add a span to it
+// And then add a generation to the span
+func main() {
+	//logTrace(nil)
+	logTraceWithSession()
 	logger.Flush()
 }
